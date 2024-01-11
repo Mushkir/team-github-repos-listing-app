66fe69215592dc7ac0a04e1bb64c5dddaadb7bd7
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-01-11 11:11:50 +0530

    feat: responsive feature added

---
+++ README.md
@@ -0,0 +1,62 @@
+# Cyberdude Challenges - DOM (Document Object Model)
+
+## Task 05: Listing all repositories of Team Members' GitHub Account
+
+
+
+### Storyline
+
+// Todo: Storyline
+// Todo: 1. Need to get team GitHub username
+// Todo: 2. Integrate GitHub API
+// Todo: 3. Need to add username with `https://api.github.com/users/username`
+// Todo: 4. Display the data from API to UI
+
+
+### Live link
+
+https://mushkir.github.io/youtube-thumbnail-downloader-app/
+
+
+
+## Technologies and Libraries Used and Learned
+
+| s.no | Technologies and Libraries Used                                                  |
+| :--- | -------------------------------------------------------------------------------- |
+| 01   | [Javascript]                                                                     |
+| 02   | [JustValidateDev]                                                                |
+| 03   | [YouTube-URL](https://www.npmjs.com/package/youtube-url)                         |
+
+### Screenshots
+
+![Screenshot-01](./screenshots//Screenshot-01.png)
+
+
+
+![Screenshot-02](./screenshots/Screenshot-02.png)
+
+### TODOs:
+
+- [*] Form validation
+- [*] Show the URL Image
+
+## Validation rules
+
+1. Client
+
+   - Native browser validation
+   - External Javascript Libraries to validate your form.
+
+
+## Internship
+
+This internship is provided by [CyberDude Networks Pvt. Ltd.](https://youtube.com/cyberdudenetworks) as part of the 6-Month Free Internship program, a skill development initiative organized to enhance participants' skills. Mentoring was provided by [Mr. Anbuselvan Rocky](https://instagram.com/anbuselvanrocky). For more information, [you can contact CyberDude Networks here](https://cyberdudenetworks.com).
+
+
+
+### License: MIT (Do whatever, you want!)
+
+
+### Acknowledgment:
+
+- [Anbuselvan Rocky](https://fb.me/anburocky3)
---
+++ assets/js/main.js
@@ -0,0 +1,116 @@
+// Todo: Storyline
+// Todo: 1. Need to get team GitHub username
+// Todo: 2. Integrate GitHub API
+// Todo: 3. Need to add username with https://api.github.com/users/username
+// Todo: 4. Display the data from API to UI
+
+import axios from "axios";
+
+const teamUsernames = [
+  "esakki2104prsnl",
+  "vk2401",
+  "suriyamassmsd",
+  "riyaz1000",
+  "hema-venkat3",
+  "yrd369",
+  "mushkir",
+  "bearcin46",
+  "dineshdevelope",
+  "jeya-rosini",
+  "swethadsalvatore",
+  "mshajid",
+  "muthuakalya",
+  "vedhatech002",
+  "danielace1",
+  "sharif-22",
+  "gayathrihg",
+  "kishorekumar-kp",
+  "muthukumarimoorthi",
+];
+
+const username = "mushkir";
+const token = "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";
+
+teamUsernames.forEach((element) => {
+  const apiUrl = `https://api.github.com/users/${element}`;
+  axios
+    .get(apiUrl, {
+      auth: {
+        username: username,
+        password: token,
+      },
+    })
+    .then((response) => {
+      // Display the data in the UI or process it as needed
+      console.log(`Repos for ${element}:`, response.data);
+
+      const divEl = document.createElement("div");
+      divEl.id = "profile-container";
+      divEl.classList.add("px-10", "items-center", "justify-between");
+
+      divEl.innerHTML = `<div class="bg-[#FBF6EE] p-5 w-[300px] mt-[50px] rounded-md">
+<img
+  src="${response.data["avatar_url"]}"
+  class="w-[150px]  mx-auto mt-5 rounded-full object-cover border-4 border-[#142850]"
+  alt=""
+/>
+
+<div class="text-[#142850] text-center">
+  <div class="text-center mt-3">
+    <p class="font-bold text-2xl">${response.data.name}</p>
+  </div>
+
+  <div>
+   <p>${
+     response.data.location === null
+       ? "Currently their native unavailable"
+       : `📍 ${response.data.location}`
+   }</p>
+  </div>
+
+  <div class="flex gap-x-4 justify-center">
+    <div class="flex items-center gap-x-2">
+      <p class="font-bold">Following:</p>
+      <p>${
+        response.data.following < 10
+          ? `0${response.data.following}`
+          : `${response.data.following}`
+      }</p>
+    </div>
+
+    <div class="flex items-center gap-x-2">
+      <p class="font-bold">Followers:</p>
+      <p>${
+        response.data.followers < 10
+          ? `0${response.data.followers}`
+          : `${response.data.followers}`
+      }</p>
+    </div>
+  </div>
+
+  <div class="flex items-center justify-center gap-x-2">
+    <p class="font-bold">Public Repos:</p>
+    <p>${
+      response.data["public_repos"] < 10
+        ? `0${response.data["public_repos"]}`
+        : `${response.data["public_repos"]}`
+    }</p>
+  </div>
+
+  <div class="mt-5 mb-5">
+    <a
+      href= "https://github.com/${response.data.login}?tab=repositories"
+      target="_blank"
+      class="bg-[#142850] text-[#FBF6EE] px-5  py-2 rounded-md cursor-pointer hover:transition 500 hover:bg-[#425272]"
+      >View Repo</a
+    >
+  </div>
+</div>
+</div>`;
+
+      document.body.append(divEl);
+    })
+    .catch((error) => {
+      console.error(`Error fetching repos for ${element}:`, error.message);
+    });
+});
